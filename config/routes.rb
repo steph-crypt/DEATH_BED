94b1c8228fd7590fd9b80cf3584763d91cf2c78b
Lauren St Jean <lauren.stjean@gmail.com>
2019-11-19 14:12:56 +0100

    should resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   resources :plots, only: [:index, :show, :new, :create]
   resources :users, only: [:show]
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
