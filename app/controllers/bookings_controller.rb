e4a521099e3a6122a561502cafff3f8d1d30da41
Mark Slinde <slinde.mark@gmail.com>
2019-11-20 10:31:37 +0100

    switching to new branch

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def new
+    @plot = Plot.find(params[:plot_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @plot = Plot.find(params[:plot_id])
+    @booking = Booking.new(booking_params)
+    @booking.ploat = @plot
+    if @booking.save
+      redirect_to plot_path(@plot)
+    else
+      render :new
+    end
+
+    private
+
+  def booking_params
+    params.require(:booking).permit(:description)
+  end
 end
