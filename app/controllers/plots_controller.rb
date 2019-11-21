72b5ec1aba6972c9f4d7df809ae7d7b4ee7a5093
Sofia Stephenson <sgandsteph@gmail.com>
2019-11-21 16:15:15 +0100

    Pundit add. Add plot edit and new show, add application controller, application policy, plot policy

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,16 +1,40 @@
 class PlotsController < ApplicationController
   def index
-    @plots = Plot.all
+    @plots = policy_scope(Plot).order(created_at: :desc)
   end
 
   def show
     @plot = Plot.find(params[:id])
     @user = @plot.user
+    raise
+    authorize @plot
   end
 
+  def new
+    @plot = Plot.new
+  end
+
+  def create
+    authorize @plot
+    @plot = Plot.new(plot_params)
+    if @plot.save
+      redirect_to plot_path(@plot)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @plot = Plot.find(params[:id])
+  end
+
+  def update
+    @plot = Plot.find(params[:id])
+    @plot.update(params[:plot])
+  end
   # private
 
-  # def plot_params
-  #   params.require(:user).permit(:name)
-  # end
+  def plot_params
+    params.require(:user).permit(:name)
+  end
 end
