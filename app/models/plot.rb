fc58a866ee8d196c5d82da27e528a9651b279c1b
Mark Slinde <slinde.mark@gmail.com>
2019-11-21 15:58:28 +0100

    Bookings update/cleanup

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -3,4 +3,10 @@ class Plot < ApplicationRecord
   has_many :bookings
   belongs_to :user
   mount_uploader :photo, PhotoUploader
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
